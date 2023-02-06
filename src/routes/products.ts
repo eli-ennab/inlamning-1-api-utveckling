a4908094e3b9badd889198ef39c2400e23fda6cd
Eli Ennab <elinaennab@gmail.com>
2023-02-06 12:54:42 +0100

    Added validation rules for posting products.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { index, store, show} from '../controllers/product_controller'
+import { body } from 'express-validator'
 const router = express.Router()
 
 // GET /products
@@ -9,6 +10,12 @@ router.get('/', index)
 router.get('/:productId', show)
 
 // POST /products
-router.post('/', store)
+router.post('/',
+	body('name').isString(),
+	body('price').isLength( { min: 1 }),
+	body('images').isJSON(),
+	body('stock_status').isString(),
+	body('stock_quantity').isLength( { min: 0 })
+, store)
 
 export default router
