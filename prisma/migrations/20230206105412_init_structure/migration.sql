4f2265e86d83e3421b1562d481c51724025f580f
Eli Ennab <elinaennab@gmail.com>
2023-02-06 11:55:40 +0100

    New database and migrations testing if deploy will work now.

--- prisma/migrations/20230206105412_init_structure/migration.sql
+++ prisma/migrations/20230206105412_init_structure/migration.sql
@@ -17,7 +17,7 @@ CREATE TABLE `Order` (
     `customer_first_name` VARCHAR(191) NOT NULL,
     `customer_last_name` VARCHAR(191) NOT NULL,
     `customer_address` VARCHAR(191) NOT NULL,
-    `customer_postcode` VARCHAR(191) NOT NULL,
+    `customer_postcode` VARCHAR(6) NOT NULL,
     `customer_city` VARCHAR(191) NOT NULL,
     `customer_email` VARCHAR(191) NOT NULL,
     `customer_phone` VARCHAR(191) NULL,
@@ -28,10 +28,18 @@ CREATE TABLE `Order` (
 
 -- CreateTable
 CREATE TABLE `OrderItems` (
-    `product_id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+    `order_id` INTEGER UNSIGNED NOT NULL,
+    `product_id` INTEGER UNSIGNED NOT NULL,
     `qty` INTEGER UNSIGNED NOT NULL,
     `item_price` INTEGER UNSIGNED NOT NULL,
     `item_total` INTEGER UNSIGNED NOT NULL,
 
-    PRIMARY KEY (`product_id`)
+    PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderItems` ADD CONSTRAINT `OrderItems_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
