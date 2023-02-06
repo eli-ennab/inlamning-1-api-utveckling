5572a31abd5f85587bad0e4b7924d14c6c59b48a
Eli Ennab <elinaennab@gmail.com>
2023-02-06 12:40:41 +0100

    Updated SQL-dump.

---
+++ sql_dump.sql
@@ -0,0 +1,192 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: bojysihaxic4acntej5w-mysql.services.clever-cloud.com:3306
+-- Generation Time: Feb 06, 2023 at 11:37 AM
+-- Server version: 8.0.22-13
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bojysihaxic4acntej5w`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Order`
+--
+
+CREATE TABLE `Order` (
+  `id` int UNSIGNED NOT NULL,
+  `customer_first_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_last_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_address` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_postcode` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_city` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `customer_phone` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `order_total` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `Order`
+--
+
+INSERT INTO `Order` (`id`, `customer_first_name`, `customer_last_name`, `customer_address`, `customer_postcode`, `customer_city`, `customer_email`, `customer_phone`, `order_total`) VALUES
+(2, 'Eli', 'Kom igen', 'Mogens 10', '12345', 'Malmö', 'eli@dundermifflin.org', '0739-393939', 1),
+(3, 'Elina', 'Ennab', 'Mogensgatan 5 B', '21446', 'MALMÖ', 'elinaennab@gmail.com', '0739516398', 12),
+(4, 'Elina', 'Ennab', 'Mogensgatan 5 B', '21446', 'MALMÖ', 'elinaennab@gmail.com', '0739516398', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `OrderItems`
+--
+
+CREATE TABLE `OrderItems` (
+  `id` int UNSIGNED NOT NULL,
+  `order_id` int UNSIGNED NOT NULL,
+  `product_id` int UNSIGNED NOT NULL,
+  `qty` int UNSIGNED NOT NULL,
+  `item_price` int UNSIGNED NOT NULL,
+  `item_total` int UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `OrderItems`
+--
+
+INSERT INTO `OrderItems` (`id`, `order_id`, `product_id`, `qty`, `item_price`, `item_total`) VALUES
+(2, 2, 1, 1, 8, 8),
+(3, 3, 3, 1, 12, 12),
+(4, 4, 2, 1, 7, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product`
+--
+
+CREATE TABLE `Product` (
+  `id` int UNSIGNED NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int NOT NULL,
+  `images` json NOT NULL,
+  `stock_status` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stock_quantity` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `Product`
+--
+
+INSERT INTO `Product` (`id`, `name`, `description`, `price`, `images`, `stock_status`, `stock_quantity`) VALUES
+(1, 'Banana Bubs', '<p>Banan/gräddkola</p>\n<p>Innehållsförteckning: Glukos-fruktossirap, socker, majsstärkelse, vatten, surhetsreglerande medel (äppelsyra, natriumcitrat), potatisprotein, aromämnen, färgämnen: (E150d, E100), kokosolja, ytbehandlingsmedel (karnaubavax).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 8, '{\"large\": \"/storage/products/156622.png\", \"thumbnail\": \"/storage/products/thumbnails/156622-300x300.png\"}', 'instock', 8),
+(2, 'Colanappar', '<p>Vingummi med colasmak</p>\n<p>Innehållsförteckning: Glukossirap, socker, gelatin, druvsocker, syra: citronsyra, karamelliserat socker, arom, palmolja, ytbehandlingsmedel: bivax vitt och gult, karnaubavax.</p>\n<p>Kan innehålla VETE.</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 7, '{\"large\": \"/storage/products/173452.png\", \"thumbnail\": \"/storage/products/thumbnails/173452-300x300.png\"}', 'instock', 2),
+(3, 'Gott & Blandat Giants', '<p>En mix av lakrits och gelé med fruktsmak</p>\n<p>Innehållsförteckning: Socker, glukossirap, glukos-fruktossirap, stärkelse, VETEMJÖL, melass, syra (citronsyra), fuktighetsbevarande medel (sorbitoler, glycerol), lakritsextrakt, salt, vegetabiliska oljor (kokos, palm), aromer, färgämnen (E153, E120, E100, E141), ytbehandlingsmedel (bivax), stabiliseringsmedel (E471).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 12, '{\"large\": \"/storage/products/1997509.png\", \"thumbnail\": \"/storage/products/thumbnails/1997509-300x300.png\"}', 'instock', 5),
+(4, 'Cola Rocketz', '<p>Fylld brun lakrits med colasmak.</p>\n<p>Innehållsförteckning: Socker, glukos-fruktossirap, VETEMJÖL, surhetsreglerande medel (äppelsyra, citronsyra), helt härdat vegetabiliskt fett (palmkärna, kokos), fuktighetsbevarande medel (glycerol), emulgeringsmedel (mono- och diglycerider av fettsyror), förtjockningsmedel (arabiskt gummi), modifierad majsstärkelse, MALTEXTRAKT FRÅN KORN, aromämne, kakaopulver, sirap, salt, ytbehandlingsmedel (karnaubavax).</p>\n<p><em>Alla priser är per skopa.</em></p>\n', 8, '{\"large\": \"/storage/products/2502327.png\", \"thumbnail\": \"/storage/products/thumbnails/2502327-300x300.png\"}', 'instock', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `_prisma_migrations`
+--
+
+CREATE TABLE `_prisma_migrations` (
+  `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `checksum` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `finished_at` datetime(3) DEFAULT NULL,
+  `migration_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logs` text COLLATE utf8mb4_unicode_ci,
+  `rolled_back_at` datetime(3) DEFAULT NULL,
+  `started_at` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+  `applied_steps_count` int UNSIGNED NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `_prisma_migrations`
+--
+
+INSERT INTO `_prisma_migrations` (`id`, `checksum`, `finished_at`, `migration_name`, `logs`, `rolled_back_at`, `applied_steps_count`) VALUES
+('c982a266-b0d3-42c7-a66d-b50a5de77239', '0df18ae90b155e1028f7e9edeed4bef1d543eb97130485944e0d5d7b5c4d8a9b', '2023-02-06 11:01:44.937', '20230206105412_init_structure', NULL, NULL, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Order`
+--
+ALTER TABLE `Order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `OrderItems`
+--
+ALTER TABLE `OrderItems`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `OrderItems_order_id_fkey` (`order_id`),
+  ADD KEY `OrderItems_product_id_fkey` (`product_id`);
+
+--
+-- Indexes for table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `_prisma_migrations`
+--
+ALTER TABLE `_prisma_migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Order`
+--
+ALTER TABLE `Order`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `OrderItems`
+--
+ALTER TABLE `OrderItems`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `Product`
+--
+ALTER TABLE `Product`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `OrderItems`
+--
+ALTER TABLE `OrderItems`
+  ADD CONSTRAINT `OrderItems_order_id_fkey` FOREIGN KEY (`order_id`) REFERENCES `Order` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `OrderItems_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
