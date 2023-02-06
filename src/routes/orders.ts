316d8de286b22350d82ea1e2bbb445f8b2b8593d
Eli Ennab <elinaennab@gmail.com>
2023-02-06 11:47:47 +0100

    Changed validation rules to at least 2 chars.

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -11,11 +11,11 @@ router.get('/:orderId', show)
 
 // POST /orders
 router.post('/', [
-	body('customer_first_name').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3-191 chars long'),
-	body('customer_last_name').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3-191 chars long'),
-	body('customer_address').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3-191 chars long'),
+	body('customer_first_name').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
+	body('customer_last_name').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
+	body('customer_address').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
 	body('customer_postcode').isString().withMessage('has to be a string').bail().isLength({ max: 6 }).withMessage('has to be less than 7 chars long'),
-	body('customer_city').isString().withMessage('has to be a string').bail().isLength({ min: 3, max: 191 }).withMessage('has to be 3-191 chars long'),
+	body('customer_city').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
 	body('customer_email').isEmail().bail().withMessage('has to be an emailaddress'),
 	body('customer_phone').optional()
 ], store)
