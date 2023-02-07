75dbe3024078f283bb12976c8b6cfebff0f528b9
Eli Ennab <elinaennab@gmail.com>
2023-02-07 10:06:48 +0100

    Corrected validation rules for products.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -9,9 +9,9 @@ router.post('/', [
 	body('name').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
 	body('description').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
 	body('price').isNumeric().withMessage('has to be a number').bail().isLength({ min: 1 }).withMessage('has to at least 1'),
-	body('images').isJSON().withMessage('has to be JSON').bail(),
+	body('images').isJSON().withMessage('has to be JSON'),
 	body('stock_status').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
-	body('stock_quantity').isLength( { min: 0 } ).bail(),
+	body('stock_quantity').isLength( { min: 0 } ),
 ], store)
 
 export default router
