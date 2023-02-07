b708ed2ddbaf04ced2f82171242958062a43a210
Eli Ennab <elinaennab@gmail.com>
2023-02-07 16:34:07 +0100

    Added validation for POST/ product images.

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -8,9 +8,10 @@ router.get('/:productId', show)
 router.post('/', [
 	body('name').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
 	body('description').isString().withMessage('has to be a string').bail().isLength({ min: 2 }).withMessage('has to at least 2 chars long'),
-	body('price').isNumeric().withMessage('price has to contain a number'),
+	body('price').isInt( { min: 1 } ).withMessage('price has to be at least 1'),
+	body('images').notEmpty().withMessage('must include images'),
 	body('stock_status').isString().withMessage('has to be a string').bail(),
-	body('stock_quantity').isLength( { min: 0 } ),
+	body('stock_quantity').isInt( { min: 0 } ),
 ], store)
 
 export default router
